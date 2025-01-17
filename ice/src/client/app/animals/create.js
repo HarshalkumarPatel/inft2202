f8b9175a8e3a974ab74b63a9d811e0251670a940
HarshalkumarPatel <harshalkumar.patel@dcmail.ca>
2025-01-17 18:45:11 -0500

    ice 3

--- ice/src/client/app/animals/create.js
+++
@@ -1,7 +0,0 @@
-/*
-    Name: Harshalkumar Patel
-    filename: create.js
-    Course: INFT 2202
-    Date: January 10, 2025
-    Description: This is my general application script.  Functions that are required on every page should live here.
-*/
--- ice/src/client/app/animals/update.js
+++
@@ -1,7 +0,0 @@
-/*
-    Name: Harshalkumar Patel
-    filename: update.js
-    Course: INFT 2202
-    Date: January 10, 2025
-    Description: This is my general application script.  Functions that are required on every page should live here.
-*/
